b12fd70e0eb721c3cd294333bc3ad76f26dfd791
Miguel Gabriel <srtjmg@gmail.com>
2024-08-17 18:10:20 -0300

    removendo teste Miguel

--- serra_capivara/urls.py
+++ serra_capivara/urls.py
@@ -15,8 +15,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-# teste Miguel
-
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
