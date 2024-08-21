3cd8f769b5264ecc1b2f939ae3ddc9414638f96e
Miguel Gabriel <srtjmg@gmail.com>
2024-08-21 14:21:42 -0300

    Açucareiros + Canecas

--- serra_front/src/App.jsx
+++ serra_front/src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
