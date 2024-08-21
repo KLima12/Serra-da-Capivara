b428fc351cd41ccaa2a0aca0e5336313f20e23c6
Miguel Gabriel <srtjmg@gmail.com>
2024-08-21 11:48:50 -0300

    react build nomes padronizados

--- serra_front/src/App.jsx
+++ serra_front/src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import teste from './components/teste';
+import HelloWorld from './components/teste';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <HelloWorld />
+
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
--- serra_front/vite.config.js
+++ serra_front/vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'index.js',
+        chunkFileNames: 'index.js',
+        assetFileNames: ({ name }) => {
+          if (name && name.endsWith('.css')) {
+            return 'index.css';
+          }
+          return '[name].[ext]';
+        },
+      },
+    },
+  },
+});
