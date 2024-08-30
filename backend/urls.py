3111fc0f195bdec1455087e3c5c5b7165b7e59f0
Miguel Gabriel <srtjmg@gmail.com>
2024-08-30 16:11:21 -0300

    Carrinho pt2

--- backend/urls.py
+++ backend/urls.py
@@ -7,6 +7,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.inicio, name='inicio'),
     path('produtos/', views.produtos, name='produtos'),
+    path('carrinho/', views.carrinho, name='carrinho'),
+    path('get-cart-items/', views.get_cart_items, name='get_cart_items'),
     path('save_cart/', views.save_cart, name='save_cart'),
     path('message/', views.message, name='message'),
 ]
--- backend/views.py
+++ backend/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, HttpResponse, redirect
 from gestao.models import Product, Category
 from urllib.parse import quote
 from django.http import JsonResponse
+from django.core.serializers import serialize
 import json
 
 def inicio(request):
@@ -22,6 +23,74 @@ def produtos(request):
     }
 
     return render(request, 'produtos.html', context)
+
+
+def carrinho(request):
+    categories = Category.objects.all().order_by('name')
+    products = Product.objects.all()
+    category_products = {
+        category: Product.objects.filter(category=category).order_by('name')
+        for category in categories
+    }
+
+    cart = []
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        cart = data.get('cart', [])
+
+    if request.method == 'GET':
+        cart = [] 
+
+    products_data = [
+        {
+            "pk": product.pk,
+            "fields": {
+                "name": product.name,
+                "code": product.code,
+                "size": product.size,
+                "views": product.views,
+            }
+        }
+        for product in products
+    ]
+
+    context = {
+        'categories': categories,
+        'products_json': json.dumps(products_data), 
+        'category_products': category_products,
+        'cart': cart
+    }
+
+    return render(request, 'carrinho.html', context)
+
+
+
+def get_cart_items(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        cart_items = data.get('cart', [])
+        product_ids = [item['id'] for item in cart_items]
+        products = Product.objects.filter(id__in=product_ids)
+
+        products_data = []
+        for product in products:
+            cart_item = next((item for item in cart_items if item['id'] == product.id), None)
+            products_data.append({
+                'id': cart_item['id'],
+                'name': product.name,
+                'code': product.code,
+                'size': product.size,
+                'views': product.views,
+                'photos': [photo.url for photo in product.photos.all()],
+                'quantity': cart_item['quantity'] if cart_item else 0,
+            })
+
+        return JsonResponse({'products': products_data})
+
+    return JsonResponse({'error': 'Invalid request method'}, status=400)
+
+    
+
 
 def save_cart(request):
     if request.method == 'POST':
