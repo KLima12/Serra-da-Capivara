9e78af414de1e16d657906dc703d745277b13e90
Miguel Gabriel <srtjmg@gmail.com>
2024-12-03 20:28:16 -0300

    Rodapé

--- serra_front/src/components/footer.jsx
+++ serra_front/src/components/footer.jsx
@@ -1,27 +1,71 @@
 import React from "react";
 
 const Footer = () => {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
   return (
-    <div>
-      <hr></hr>
-      <section>
-        <h2>Central de Atendimento</h2>
-        <h4>
-          Telefone para vendas: <br></br>(89) 98119-0133
-        </h4>
+    <div class="footer">
+      <hr class="footerLine"></hr>
+      <div class="footerSections">
+        <section>
+          <h2>Central de Atendimento</h2>
 
-        <h4>
-          Fábrica e Escritório: <br></br>(89) 99433-2021
-        </h4>
+          <br></br>
 
-        <h4>
-          Loja São Raimundo Nonato – PI: <br></br>(89) 3582-1949
-        </h4>
+          <h4>
+            Telefone para vendas:
+            <br></br>
+            <h5>(89) 98119-0133</h5>
+          </h4>
 
-        <h4>
-          Email: <br></br>vendas@ceramicacapivara.com
-        </h4>
-      </section>
+          <br></br>
+
+          <h4>
+            Fábrica e Escritório:
+            <br></br>
+            <h5>(89) 99433-2021</h5>
+          </h4>
+
+          <br></br>
+
+          <h4>
+            Loja São Raimundo Nonato – PI:
+            <br></br>
+            <h5>(89) 3582-1949</h5>
+          </h4>
+
+          <br></br>
+
+          <h4>
+            Email:
+            <br></br>
+            <h5>vendas@ceramicacapivara.com</h5>
+          </h4>
+        </section>
+
+        <section class="sectionInstagram">
+          <h2>Siga-nos no Instagram!</h2>
+          <a
+            href="https://www.instagram.com/ceramicaserradacapivarapi/"
+            target="blank"
+          >
+            <img
+              src="../../media/icon/instagram.png"
+              alt="logo instagram"
+              class="logoInstagram"
+            ></img>
+          </a>
+        </section>
+      </div>
+
+      <button class="footerButton" onClick={scrollToTop}>
+        Voltar ao topo
+      </button>
     </div>
   );
 };
